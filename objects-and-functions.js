83d649ddabdd9aab80cdd0786a24c23fe73c3e90
davieu <davisumana@gmail.com>
2018-09-13 14:52:14 -0400

    lesson challenge complete

--- objects-and-functions.js
+++ objects-and-functions.js
@@ -1,4 +1,12 @@
 /*
+//PERSONAL NOTES
+pareInt() converts string into number
+// var answer = parseInt(prompt('Please select the right answer.'));
+*/
+
+////////////////////////////////////////////////////////
+
+/*
 //FUNCTION CONSTRUCTORS
 var Person = function(name, dateOfBirth, job) {
     this.name = name;
@@ -313,9 +321,9 @@ console.log(fullkiwi)
 */
 
 ///////////////////////////////////////////////////////////////////////
-//QUESTIONAIRE CHALLENGE
 
 /*
+//QUESTIONAIRE CHALLENGE
 //THIS is my version of the challenge. a questionaire that moves to the next random question and keeps score.
 var score = 0;
 var initial = 0;
@@ -381,32 +389,124 @@ function questionAnswers(questionParam) {
         questionAnswers(randomQuestion);
     }
 }
-
 questionAnswers(randomQuestion)
 */
 
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 //LESSON version of the questionaire
-
 //build function constructor
-function Question(question, answers, correct) {
-    this.question = question;
-    this.answers = answers;
-    this.correct = correct;
-}
 
-var q1 = new Question('Is Javascript the coolest programming language in the world?', 
-                     ['Yes', 'No'],
-                     0);
-var q2 = new Question('What\'s the name of the course\'s teacher?',
-                    ['John', 'Micheal', 'Jonas'],
-                    2);
-var q3 = new Question('What best describes coding?', 
-                    ['Boring', 'Hard', 'Fun', 'Tedious'],
-                    2)
+/*
+--- Let's build a fun quiz game in the console! ---
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+2. Create a couple of questions using the constructor
+3. Store them all inside an array
+4. Select one random question and log it on the console, together with the possible answers (each 
+   question should have a number) (Hint: write a method for the Question objects for this task).
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input 
+   the number of the correct answer such as you displayed it on Task 4.
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is
+   private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+   IIFE immidiately invoked function expression will do this
+*/
 
-var questions = [q1, q2, q3];
-var n = Math.floor(Math.random() * questions.length);
-console.log(n);
+/*
+--- Expert level ---
+8. After you display the result, display the next random question, so that the game never ends (Hint: write a function for this and call it right after displaying the result)
+9. Be careful: after Task 8, the game literally never ends. So include the option to quit the game if the user writes 'exit' instead of the answer. In this case, DON'T call the function from task 8.
+10. Track the user's score to make the game more fun! So each time an answer is correct, add 1 point to the score (Hint: I'm going to use the power of closures for this, but you don't have to, just do this with the tools you feel more comfortable at this point).
+11. Display the score in the console. Use yet another method for this.
+*/
+
+//IIFE creates new scope wich defines all the code that is in the function. If someone else includes this code in their project
+//and also has a variable called question, answer or n. Their code will not interfere with this code. No other code will overwrite this code
+(function() {
+    function Question(question, answers, correct) {
+        this.question = question;
+        this.answers = answers;
+        this.correct = correct;
+    };
+    
+    Question.prototype.displayQuestion = function() {
+        console.log(this.question);
+    
+        for (var i = 0; i < this.answers.length; i++) {
+            console.log(i + ': ' +  this.answers[i]);
+        }
+    }
+    
+    Question.prototype.checkAnswer = function(ans, callback) {
+        var sc;
+        if (ans === this.correct) {
+            console.log('\n' + ans + ' is correct answer')
+            sc = callback(true);
+        } else {
+            console.log('\n' + ans + ' is incorrect. Please try again')
+            sc = callback(false);
+        }
+        this.displayScore(sc);
+    }
+
+    Question.prototype.displayScore = function(score) {
+        console.log('your current score is: ' + score);
+        console.log('---------------------------------------------------------------')
+    };
+
+    
+    var q1 = new Question('Is Javascript the coolest programming language in the world?', 
+                         ['Yes', 'No'],
+                         0);
+    var q2 = new Question('What\'s the name of the course\'s teacher?',
+                        ['John', 'Micheal', 'Jonas'],
+                        2);
+    var q3 = new Question('What best describes coding?', 
+                        ['Boring', 'Hard', 'Fun', 'Tedious'],
+                        2);
+    
+    var questions = [q1, q2, q3];        
+    
+    console.log('Game begins!\n\n')
+
+    //CLOSURE FUNCTION - call score function
+    function score() {
+        var sc = 0;
+        //once score is called it returns function below
+        return function(correct) {
+            if (correct) {
+                sc++;
+            } else {
+                sc--
+            }
+            return sc;
+        }
+    }
+
+    var keepScore = score();
+
+    function nextQuestion() {
+        
+        var n = Math.floor(Math.random() * questions.length);
+        
+        questions[n].displayQuestion();
+        
+        //parseInt() convert string to a number. prompts always evaluate to string.
+        var answer = prompt('\nPlease select the right answer.');
+        if (answer === 'exit' || answer === null) {
+            console.log('---------------------------------------------------------------\nYou quit!\n'+ 'Final score: ' + [keepScore() + 1] + '\n---------------------------------------------------------------');
+        } else {
+            questions[n].checkAnswer(parseInt(answer), keepScore);
+            nextQuestion();
+        }
+    }
+    nextQuestion();
+})();
+
+
 
 
 
