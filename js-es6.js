a0b56c111fa8ae135cf210b61ab313cfecf177ef
davieu <davisumana@gmail.com>
2018-09-19 23:46:56 -0400

    es6 IIFEs or blocks initial commit

---
+++ js-es6.js
@@ -0,0 +1,20 @@
+/*********************************************************
+ * block and IIFEs
+ */
+
+ {
+     const a = 1;
+     let b = 2; 
+     var c = 3;
+ }
+
+//  console.log(a + b);
+console.log(c);
+
+ //ES5 
+
+ (function() {
+     var c = 3;
+ })();
+
+ console.log(c;)
