43fef94ad08a903f0764db8c501b4b64ecb0273e
davieu <davisumana@gmail.com>
2018-09-20 23:31:58 -0400

    test

--- js-es6.js
+++ js-es6.js
@@ -1,36 +1,3 @@
-/*********************************************************
- * block and IIFEs
+/***********************************************************
+ * es6
  */
-
- /*
- {
-     const a = 1;
-     let b = 2; 
-     var c = 3;
- }
-
-//  console.log(a + b);
-console.log(c);
-
- //ES5 
-
- (function() {
-     var c = 3;
- })();
-
-//  console.log(c);
-*/
-
-let firstName = 'john';
-let lastName = 'smith';
-const yearOfBirth = 1990;
-
-function calcAge(year) {
-    return 2016 - year;
-}
-
-//ES5
-console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
-
-//ES6
-console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
