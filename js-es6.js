163d7ef28786e579a6e51636d8aadcdc3376d005
davieu <davisumana@gmail.com>
2018-10-02 14:39:40 -0400

    arrow  functions

--- js-es6.js
+++ js-es6.js
@@ -122,3 +122,40 @@ console.log(firstName.repeat(3))
 console.log(`${firstName} `.repeat(3))
 console.log(n);
 */
+
+
+/*******************************************************************************
+ * ARROW FUNCTIONS
+ */
+
+ /*
+const years = [1990, 1965, 1982, 1937];
+
+ //es5
+var ages5 = years.map(function(el) {
+    return 2016 - el
+});
+console.log(ages5);
+
+
+//es6 longer version
+const ages66 = years.map((el) => {
+    return 2016 - el;
+});
+console.log(ages66);
+
+
+//es6
+let ages6 = years.map(el => 2016 - el)
+console.log(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2016 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+    const now = new Date().getFullYear();
+    const age = now - el;
+    return `Age element ${index + 1}: ${age}`
+});
+console.log(ages6);
+*/
