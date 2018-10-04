3b6b19b93fd9327a04f0d7b5166ba6d465e86a5a
davieu <davisumana@gmail.com>
2018-10-04 11:27:02 -0400

    es6 spread operator

--- js-es6.js
+++ js-es6.js
@@ -307,8 +307,8 @@ console.log(retirement);
 
 //Array.from() *********  makes a nodelist gathered from the html into an array
 
+/*
  //es5
-
  const boxes = document.querySelectorAll('.box');
 
  var boxesArr5 = Array.prototype.slice.call(boxes);
@@ -330,6 +330,7 @@ console.log(retirement);
 //  boxesArr6.map((cur) => {
 //      cur.style.backgroundColor = 'red';
 //  })
+*/
 
 
 //es5
@@ -345,6 +346,7 @@ for (var i = 0; i < boxesArr5.length; i++) {
 */
 
 
+/*
 //es6
 for (const cur of boxesArr6) {
     if (cur.className.includes('blue')) {
@@ -368,3 +370,46 @@ console.log(ages[full.indexOf(true)]);  //21
 //has access to the current element, current index, and whole array
 console.log(ages.findIndex(cur => cur >= 18));  //3
 console.log(ages.find(cur => cur >= 18))  //21
+*/
+
+
+/**************************************************************************
+ * SPREAD OPERATOR
+ */
+
+ /*
+ addFourAges = (a, b, c, d) => {
+     return a + b + c + d;
+ }
+
+ var sum1 = addFourAges(18, 30, 12, 21);
+ console.log(sum1);
+
+ //es5
+var ages = [18, 30, 12, 21];
+var sum2 = addFourAges.apply(null, ages);;
+console.log(sum2);
+
+
+//es6
+const sum3 = addFourAges(...ages);
+console.log(sum3)
+
+
+const familySmith = ['john', 'jane', 'mark'];
+const familyMark = ['mary', 'bob', 'ann'];
+const bigFamily = [...familySmith, 'lily', ...familyMark]
+console.log(bigFamily);
+
+const h = document.querySelector('h1');
+const boxes = document.querySelectorAll('.box');
+const all = [h, ...boxes];
+
+Array.from(all).forEach(cur => cur.style.color = 'purple');
+console.log(all);
+*/
+
+
+/*************************************************************
+ * REST PARAMETERS
+ */
