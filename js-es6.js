ff813194fd3f99034b18e1cde46f029c98c47f8b
davieu <davisumana@gmail.com>
2018-10-04 17:52:42 -0400

    rest parameters es6

--- js-es6.js
+++ js-es6.js
@@ -413,3 +413,50 @@ console.log(all);
 /*************************************************************
  * REST PARAMETERS
  */
+
+ /*
+ //es5 
+function isFullAge5() {
+    // console.log(arguments);
+    var argsArr = Array.prototype.slice.call(arguments);
+
+    argsArr.forEach((cur) => {
+        console.log((2016 - cur) >= 18)
+    });
+}
+
+//  isFullAge5(1990, 1999, 1965, 2016, 1987);
+
+ //es6
+ isFullAge6 = (...years) => {
+    console.log(years);
+    years.forEach(cur => console.log((2016 - cur) >= 18))
+ }  
+
+ isFullAge6(1990, 1999, 1965, 2016, 1987);
+*/
+
+
+
+//ES$ with a parameter. You have to exclude first one from the array
+function isFullAge5(limit) {
+    // console.log(arguments);
+    //(arguments, 1) excludes the first paramater in the array 
+    var argsArr = Array.prototype.slice.call(arguments, 1);
+    console.log(argsArr);
+
+    argsArr.forEach((cur) => {
+        console.log((2016 - cur) >= limit)
+    });
+}
+//21 will be excluded
+ isFullAge5(21, 1990, 1999, 1965);
+
+
+ //ES6 with parameter the first one in array is the paramater
+ isFullAge6 = (limit, ...years) => {
+    console.log(years);
+    years.forEach(cur => console.log((2016 - cur) >= limit))
+ }  
+
+ isFullAge6(16, 1990, 1999, 1965, 2016, 1987);
