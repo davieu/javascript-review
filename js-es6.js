ba9f32596efa508f19847b2242a4bd89cac20b73
davieu <davisumana@gmail.com>
2018-10-05 12:54:58 -0400

    es6 map datastructure

--- js-es6.js
+++ js-es6.js
@@ -502,5 +502,42 @@ function isFullAge5(limit) {
 
 
   /*****************************************************************************
-   * MAPS
+   * MAP DATASTRUCTURE
    */
+
+const question = new Map();
+question.set('question', 'What is the new official name of the latest major javascript version?');
+question.set(1, 'es5');
+question.set(2, 'es6');
+question.set(3, 'es2015');
+question.set(4, 'es7');
+question.set('correct', 3);
+question.set(true, 'correct answer');
+question.set(false, 'wrong, please try again');
+
+console.log(question.get('question'));
+console.log(question.size);
+
+//has checks if certain key is in map
+if (question.has(4)) {
+    console.log('answer 4 is here')
+}
+
+//deletes everything in the map
+// question.clear();
+
+// question.forEach((value, key) => console.log(`this is ${key}, and it's set to ${value}`));
+
+for (let [key, value] of question.entries()) {
+    // console.log(`this is ${key}, and it's set to ${value}`);
+    if (typeof(key) === 'number') {
+        console.log(`answer ${key}: ${value}`);
+    }
+    if(typeof(key) === 'boolean') {
+        console.log(`boolean ${key}`);
+    }
+}
+
+const ans = parseInt(prompt('write the correct answer'));
+
+console.log(question.get(ans === question.get('correct')));
